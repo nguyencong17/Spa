0d672b960d61122f286e989cafaef48f50526de1
nguyencong17 <83632061+nguyencong17@users.noreply.github.com>
2022-03-24 00:31:36 +0700

    fix

---
+++ assets/index.js
@@ -0,0 +1,77 @@
+$('.carousel-1 .owl-carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: false,
+  dots: true,
+  loop: false,
+  margin: 30,
+  responsive:{
+      0:{
+          items:1,
+      },
+      768:{
+          items:2,
+      },
+      992:{
+          items:3,
+      }
+  }
+})
+
+$('.carousel-3 .owl-carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: false,
+  dots: true,
+  loop: false,
+  margin: 38,
+  responsive:{
+      0:{
+          items:1,
+      },
+      768:{
+          items:1,
+      },
+      992:{
+          items:2,
+      }
+  }
+})
+
+$('.tiptrick-carousel .owl-carousel').owlCarousel({
+  loop:true,
+  responsiveClass:true,
+  nav: false,
+  dots: true,
+  loop: false,
+  margin: 30,
+  responsive:{
+      0:{
+          items:1,
+      },
+      768:{
+          items:2,
+      },
+      992:{
+          items:3,
+      }
+  }
+})
+
+$('#owl-section-3').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+      },
+      768:{
+          items:1,
+      },
+      992:{
+          items:3,
+      }
+  }
+})
+
